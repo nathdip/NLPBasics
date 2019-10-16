5655a3eeec8bd8d62955c6c69c650c8f53a86a6f
nathdipankar <dipankar.tifr@gmail.com>
2019-10-16 22:47:46 +0200

    NLP basics

---
+++ src/test.py
@@ -0,0 +1,21 @@
+myfile = open('data/test.txt')
+
+myfile
+
+myfile.read()
+
+myfile.read()
+
+myfile.seek(0)
+
+content = myfile.read()
+
+content
+
+myfile.close()
+
+myfile = open('data/test.txt')
+
+mylines = myfile.readlines()
+
+mylines
